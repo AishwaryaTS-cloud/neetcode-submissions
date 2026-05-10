04bd3a1325bcb4fcbbe4fe0086018d9f03ca287d
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2026-05-10 21:01:03 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int num : nums)
+            freq[num]++;
+
+        for(auto &it : freq){
+            if(it.second >= 2 )
+                return true;
+        }
+        return false;
+    }
+};
