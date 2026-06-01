49a6829ce26a6d4ad126791bf962e83a32a6c3ca
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2026-06-01 09:23:16 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++){
+            cnt++;
+            if(nums[i] == 0)
+                cnt = 0;
+            
+            maxi = max(cnt , maxi);
+        }
+        return maxi;
+    }
+};
