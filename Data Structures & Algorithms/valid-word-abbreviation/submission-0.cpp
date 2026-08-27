5fd007d5158fb0a140084cc2bd85ec116dce24b4
Aishwarya T S <aishwaryatsgowda02@gmail.com>
2026-08-27 09:34:25 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int i = 0;
+        int j = 0;
+    
+
+        while(i < word.size() && j < abbr.size()){
+            if(abbr[j] == '0')
+                return false;
+
+            if(isdigit(abbr[j])){
+
+                int num = 0;
+                while(j < abbr.size() && isdigit(abbr[j])){
+                    num = num * 10 + (abbr[j] - '0');
+                    j++;
+                }
+                i += num;
+            }
+            else{
+                if(word[i] != abbr[j])
+                    return false;
+
+                i++;
+                j++;
+            }
+        }
+        return i == word.size() && j == abbr.size();
+    }
+};
